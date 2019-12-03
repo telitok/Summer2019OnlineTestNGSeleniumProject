348d6af87dedb4c0c760d8b76b5a71e7d99be534
telitok <tugbaelitok@gmail.com>
2019-12-03 16:49:25 -0500

    Automation practice

--- src/test/java/pages/CalendarEventsPage.java
+++ src/test/java/pages/CalendarEventsPage.java
@@ -1,6 +1,19 @@
 package pages;
 
-public class CalendarEventsPage {
+import Utils.BrowserUtils;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class CalendarEventsPage extends BasePage{
+
+    @FindBy(css="[title='Create Calendar event']")
+    public WebElement createCalendarEvent;
+
+    public void clickToCreateCalendarEvent(){
+        BrowserUtils.waitForVisibility(createCalendarEvent,5);
+        BrowserUtils.waitForClickablility(createCalendarEvent,5);
+        createCalendarEvent.click();
+    }
 
 
 }
